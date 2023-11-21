1a0de19ccd6abd425c06c4de0754b1f0cc340b9d
karolszymonczyk <karolszymonczyk@gmail.com>
2023-11-21 17:55:11 +0100

    MLP FashionMnist classification

--- run/train.py
+++ run/train.py
@@ -1,56 +1,33 @@
 import torch
-import torch.nn.functional as F
+from torch.utils.data import DataLoader
+from torchvision import datasets, transforms
 
 from src.models.mlp import MLP
-from src.utils import batch_data
 
-# def fizz_buzz(num):
-#     if num % 15 == 0: return "FizzBuzz"
-#     if num % 3 == 0: return "Fizz"
-#     if num % 5 == 0: return "Buzz"
-#     return ""
 
-def gen_fizzbuzz_labels(size: int) -> torch.Tensor:
-    labels_map = {0: "", 1: "fizz", 2: "buzz", 3: "fizz & buzz"}
-    labels = []
-    for i in range(size):
-        if i <= 0:
-            labels.append(0)
-            continue
-        res = 0
-        if i % 3 == 0:
-            res += 1
-        if i % 5 == 0:
-            res += 2
-        labels.append(res)
-    return torch.tensor(labels), labels_map
-
-def get_fizzbuzz_data(size: int) -> torch.Tensor:
-    x = torch.arange(0, size).unsqueeze(1)
-    y, y_map = gen_fizzbuzz_labels(size)
-
-    num_classes=len(y_map.keys())
-    y_encoded = F.one_hot(y, num_classes=num_classes).float()
-    return x, y_encoded, y_map
 
 def train() -> None:
-    x, y, y_map = get_fizzbuzz_data(100)
-    num_classes = len(y_map.keys())
+    transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5,), (0.5,))])
+
+    train_dataset = datasets.FashionMNIST(root='./data', train=True, download=True, transform=transform)
+    test_dataset = datasets.FashionMNIST(root='./data', train=False, download=True, transform=transform)
+
+    train_loader = DataLoader(dataset=train_dataset, batch_size=64, shuffle=True)
+    test_loader = DataLoader(dataset=test_dataset, batch_size=64, shuffle=False)
+
+    # Set input size based on the Fashion MNIST images (28x28)
+    input_size = 28 * 28
+    hidden_size = 128
+    num_classes = 10  # Number of classes in Fashion MNIST
     lr = 1e-3
-    batch_size = 100
-    epochs = 10_000
-    model = MLP(input_size=1, hidden_size=256, output_size=num_classes, lr=lr)
 
-    history = []
+    model = MLP(input_size=input_size, hidden_size=hidden_size, output_size=num_classes, lr=lr)
+
+    epochs = 5
     for epoch in range(epochs):
-        epoch_loss = 0
-        for batch_x, batch_y in zip(batch_data(x, batch_size), batch_data(y, batch_size)):
-            loss = model.train(batch_x.float(), batch_y.float())
-            epoch_loss += loss
-        history.append(epoch_loss)
-        if epoch % 1000:
-            print(epoch_loss)
-
+        for images, labels in train_loader:
+            loss = model.train(images, labels)
+        print(f'Epoch [{epoch + 1}/{epochs}], Loss: {loss:.4f}')
 
 if __name__ == "__main__":
     train()
--- src/models/mlp.py
+++ src/models/mlp.py
@@ -1,22 +1,24 @@
 import torch
+import torch.nn as nn
 
-
-class MLP:
+class MLP(nn.Module):
     def __init__(self, input_size: int, hidden_size: int, output_size: int, lr: float = 1e-3) -> None:
+        super().__init__()
         self.model = torch.nn.Sequential(
-            torch.nn.Linear(input_size, hidden_size),
-            torch.nn.ReLU(),
-            torch.nn.Linear(hidden_size, output_size),
+            nn.Flatten(),
+            nn.Linear(input_size, hidden_size),
+            nn.ReLU(),
+            nn.Linear(hidden_size, output_size)
         )
         self.criterion = torch.nn.CrossEntropyLoss()
         self.optimizer = torch.optim.Adam(self.model.parameters(), lr=lr)
 
-    def forward(self, X: torch.Tensor) -> torch.Tensor:
-        return self.model(X)
-    
-    def train(self, X: torch.Tensor, y: torch.Tensor) -> torch.Tensor:
-        preds = self.forward(X)
-        loss = self.criterion(preds, y)
+    def forward(self, images: torch.Tensor) -> torch.Tensor:
+        return self.model(images)
+
+    def train(self, images: torch.Tensor, lables: torch.Tensor) -> torch.Tensor:
+        preds = self.forward(images)
+        loss = self.criterion(preds, lables)
         self.optimizer.zero_grad()
         loss.backward()
         self.optimizer.step()
